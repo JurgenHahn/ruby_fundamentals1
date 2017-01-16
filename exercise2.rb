ff7781c75757eb4a551e0ccdb077f356bb4890b0
JurgenHahn <jurgenehahn@gmail.com>
2017-01-16 14:46:53 -0500

    exercise 2 done

---
+++ exercise2.rb
@@ -0,0 +1,17 @@
+#1
+
+GOOD_TIP = 0.2
+
+meal = 55
+
+puts "Exercise 1: If my meal costs $55, a good tip to leave would be $#{meal * GOOD_TIP}."
+
+#2
+
+puts "Exercise 2: " + 2.to_s + " Fly for a white guy."
+
+quotient = 45628 * 7839
+
+puts "Exercise 3: #{quotient} is a really big number."
+
+puts "Exercise 4: True."
